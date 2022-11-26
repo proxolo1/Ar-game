c72e87108e676ab66e1194cbfcf82b716775dc70
Ajay K Santhosh <73517233+proxolo1@users.noreply.github.com>
2022-11-26 14:40:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const shoot = () => {
+    const bullet = document.createElement("a-sphere");
+    let pos = myCamera.getAttribute("position");
+    bullet.setAttribute("position", pos);
+    bullet.setAttribute("velocity", getDirection(myCamera, 30));
+    bullet.setAttribute("dynamic-body", true);
+    bullet.setAttribute("radius", 0.5);
+    bullet.setAttribute("src", "https://i.imgur.com/H8e3Vnu.png");
+    let d=document.createElement("a-text");
+    d.value="hello";
+    
+    myScene.appendChild(bullet);
+  };
+  
+  document.onkeydown = event => {
+    if (event.which == 32) {
+      shoot();
+    }
+  };
+  
